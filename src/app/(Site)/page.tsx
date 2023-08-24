177a60ba0707543562815612b5cc0c28de40ea79
Juya <juya.pirouz@gmail.com>
2023-08-24 19:52:47 +0330

    logo in home page

--- src/app/(Site)/page.tsx
+++ src/app/(Site)/page.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
+import Image from "next/image";
+import React from "react";
 export default function Home() {
   return (
-    <div>Dev</div>
-  )
+    <div className=" flex flex-col min-h-full justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-full">
+        <Image
+          alt="Logo"
+          height="80"
+          width="80"
+          className="mx-auto"
+          src="/img/logo.svg"
+        ></Image>
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      {/* Form */}
+    </div>
+  );
 }
